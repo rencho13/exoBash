4a89a12a03981c19b103a7914135b06c17af63ef
rencho13 <stephone13@gmail.com>
2018-12-22 00:56:36 +0100

    création d'une fonction appréciation

--- exo1.sh
+++ exo1.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
- 
-note=0
-moyenne=0
-i=0
- 
-until [ $note -lt 0 ]
-do
-    echo "Entrez votre note (q pour quitter) :"
-    read note
-    if [ $note = "q" ]
-    then
-        note=-1
-        echo "au revoir !"
-    elif [ $note -ge 16 ]
+ppreciation()
+{
+    if [ $note -ge 16 ]
     then
         echo "très bien"
     elif [ $note -ge 14 ]
@@ -24,13 +13,11 @@ do
     elif [ $note -ge 10 ]
     then
         echo "moyen"
-    elif [ $note -ge 0 ]
     then
         echo "insuffisant"
-    else
-        echo "au revoir !"
     fi
-     
+}
+
     if [ $note -ge 0 ]
     then
         let moyenne=$moyenne+$note
